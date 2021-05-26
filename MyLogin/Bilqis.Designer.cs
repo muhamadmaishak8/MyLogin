36f8bc62bd6af9af9c38d51fcf6b02b24d3a49a9
Muhamad Maishak <0320200014@polman.astra.ac.id>
2021-05-26 22:47:11 +0700

    Ubah

--- MyLogin/Bilqis.Designer.cs
+++ MyLogin/Bilqis.Designer.cs
@@ -33,19 +33,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(436, 142);
+            this.button1.Location = new System.Drawing.Point(264, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 39);
+            this.button1.Size = new System.Drawing.Size(112, 31);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "YANG KLIK ...";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Bilqis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Bilqis";
             this.Text = "Bilqis";
             this.ResumeLayout(false);
